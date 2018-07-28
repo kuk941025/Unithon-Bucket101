1a83b375642f96b857c544f9d97ef9dc8a899ba0
Sung-Jin <osj4872@gmail.com>
2018-07-28 21:16:15 +0900

    naver login

--- app/src/main/java/com/example/jkpark/bucketlist_unit/MainActivity.java
+++ app/src/main/java/com/example/jkpark/bucketlist_unit/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.jkpark.bucketlist_unit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.jkpark.bucketlist_unit.global.GlobalVar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GlobalVar global;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        global = (GlobalVar)getApplicationContext();
+
+        Log.d("email1234", global.email);
     }
 }
