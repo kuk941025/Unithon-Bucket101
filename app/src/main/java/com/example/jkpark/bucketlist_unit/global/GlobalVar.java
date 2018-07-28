49cee620f3c520590d058b9397a47bbcc1527b90
Sung-Jin <osj4872@gmail.com>
2018-07-29 04:01:08 +0900

    editor and buchekt view function done.

--- app/src/main/java/com/example/jkpark/bucketlist_unit/global/GlobalVar.java
+++ app/src/main/java/com/example/jkpark/bucketlist_unit/global/GlobalVar.java
@@ -1,9 +1,11 @@
 package com.example.jkpark.bucketlist_unit.global;
 
 import android.app.Application;
+import android.net.Uri;
 
 public class GlobalVar extends Application{
 
     public String email;
 
+    public Uri uri;
 }
