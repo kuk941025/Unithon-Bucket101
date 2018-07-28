0b2728f327cabc31d72cf7b0cb4a6e8efcbb64c9
Sung-Jin <osj4872@gmail.com>
2018-07-29 00:35:47 +0900

    Add Editor

--- app/src/main/java/com/example/jkpark/bucketlist_unit/activity/MakeBucketActivity.java
+++ app/src/main/java/com/example/jkpark/bucketlist_unit/activity/MakeBucketActivity.java
@@ -1,25 +1,257 @@
 package com.example.jkpark.bucketlist_unit.activity;
 
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.jkpark.bucketlist_unit.R;
+import com.example.jkpark.bucketlist_unit.global.GlobalVar;
+import com.github.irshulx.Editor;
+import com.github.irshulx.EditorListener;
+import com.github.irshulx.models.EditorTextStyle;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import jp.wasabeef.richeditor.RichEditor;
 
 
 public class MakeBucketActivity extends AppCompatActivity {
-    private RichEditor editor;
+
+    Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_bucket);
 
+        editor = (Editor) findViewById(R.id.editor);
+        setUpEditor();
+    }
 
+    private void setUpEditor() {
+        findViewById(R.id.action_h1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.updateTextStyle(EditorTextStyle.H1);
+            }
+        });
+
+        findViewById(R.id.action_h2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.updateTextStyle(EditorTextStyle.H2);
+            }
+        });
+
+        findViewById(R.id.action_h3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.updateTextStyle(EditorTextStyle.H3);
+            }
+        });
+
+        findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.updateTextStyle(EditorTextStyle.BOLD);
+            }
+        });
+
+        findViewById(R.id.action_Italic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.updateTextStyle(EditorTextStyle.ITALIC);
+            }
+        });
+
+        findViewById(R.id.action_indent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.updateTextStyle(EditorTextStyle.INDENT);
+            }
+        });
+
+        findViewById(R.id.action_outdent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.updateTextStyle(EditorTextStyle.OUTDENT);
+            }
+        });
+
+        findViewById(R.id.action_bulleted).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.insertList(false);
+            }
+        });
+
+        findViewById(R.id.action_unordered_numbered).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.insertList(true);
+            }
+        });
+
+        findViewById(R.id.action_hr).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.insertDivider();
+            }
+        });
+
+        findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.openImagePicker();
+            }
+        });
+
+        findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.insertLink();
+            }
+        });
+
+        findViewById(R.id.action_erase).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.clearAllContents();
+            }
+        });
+        //editor.dividerBackground=R.drawable.divider_background_dark;
+        //editor.setFontFace(R.string.fontFamily__serif);
+        Map<Integer, String> headingTypeface = getHeadingTypeface();
+        Map<Integer, String> contentTypeface = getContentface();
+        editor.setHeadingTypeface(headingTypeface);
+        editor.setContentTypeface(contentTypeface);
+        editor.setDividerLayout(R.layout.tmpl_divider_layout);
+        editor.setEditorImageLayout(R.layout.tmpl_image_view);
+        editor.setListItemLayout(R.layout.tmpl_list_item);
+        //editor.StartEditor();
+        editor.setEditorListener(new EditorListener() {
+            @Override
+            public void onTextChanged(EditText editText, Editable text) {
+                // Toast.makeText(EditorTestActivity.this, text, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUpload(Bitmap image, String uuid) {
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+                String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+                Toast.makeText(MakeBucketActivity.this, uuid, Toast.LENGTH_LONG).show();
+                editor.onImageUploadComplete(encoded, uuid);
+                // editor.onImageUploadFailed(uuid);
+            }
+        });
+        editor.render();  // this method must be called to start the editor
+
+        findViewById(R.id.btnRender).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                Retrieve the content as serialized, you could also say getContentAsHTML();
+                */
+                String text = editor.getContentAsSerialized();
+                Log.d("test", text);
+
+                //text안에 데이터들이 싹다 저장이 됨. 이걸 이제 서버에 저장하면 되는거임!
+            }
+        });
+    }
+
+    public static void setGhost(Button button) {
+        int radius = 4;
+        GradientDrawable background = new GradientDrawable();
+        background.setShape(GradientDrawable.RECTANGLE);
+        background.setStroke(4, Color.WHITE);
+        background.setCornerRadius(radius);
+        button.setBackgroundDrawable(background);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == editor.PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                editor.insertImage(bitmap);
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            //Write your code if there's no result
+            Toast.makeText(getApplicationContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+            // editor.RestoreState();
+        } else if (requestCode == editor.MAP_MARKER_REQUEST) {
+            editor.insertMap(data.getStringExtra("cords"));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit Editor?")
+                .setMessage("Are you sure you want to exit the editor?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setGhost((Button) findViewById(R.id.btnRender));
+    }
+
+    public Map<Integer, String> getHeadingTypeface() {
+        Map<Integer, String> typefaceMap = new HashMap<>();
+        typefaceMap.put(Typeface.NORMAL, "fonts/GreycliffCF-Bold.ttf");
+        typefaceMap.put(Typeface.BOLD, "fonts/GreycliffCF-Heavy.ttf");
+        typefaceMap.put(Typeface.ITALIC, "fonts/GreycliffCF-Heavy.ttf");
+        typefaceMap.put(Typeface.BOLD_ITALIC, "fonts/GreycliffCF-Bold.ttf");
+        return typefaceMap;
+    }
+
+    public Map<Integer, String> getContentface() {
+        Map<Integer, String> typefaceMap = new HashMap<>();
+        typefaceMap.put(Typeface.NORMAL, "fonts/Lato-Medium.ttf");
+        typefaceMap.put(Typeface.BOLD, "fonts/Lato-Bold.ttf");
+        typefaceMap.put(Typeface.ITALIC, "fonts/Lato-MediumItalic.ttf");
+        typefaceMap.put(Typeface.BOLD_ITALIC, "fonts/Lato-BoldItalic.ttf");
+        return typefaceMap;
     }
 
 }
